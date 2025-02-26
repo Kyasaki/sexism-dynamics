e8fc0186b879f0de6ba4d802b85cce26fad794bb
kyasaki <zede-master@outlook.com>
2025-02-26 05:52:40 +0100

    Configure deployment for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
+import preact from '@preact/preset-vite';
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
+const repoName = process.env.GITHUB_REPOSITORY
+  ? process.env.GITHUB_REPOSITORY.split('/')[1] // Extrait uniquement REPO_NAME
+  : '';
+
 export default defineConfig({
   plugins: [preact()],
+  base: process.env.GITHUB_ACTIONS ? `/${repoName}/` : '/',
 })
